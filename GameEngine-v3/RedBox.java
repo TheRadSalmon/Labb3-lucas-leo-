6d6f506f3dc8412197106c79132390f3a84f8f88
TheRadSalmon <lucasamadeus2004@gmail.com>
2025-02-03 10:15:53 +0100

    Create RedBox.java

---
+++ GameEngine-v3/RedBox.java
@@ -0,0 +1,8 @@
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+public class RedBox extends ColoredBox {
+	public RedBox(int x, int y, int width, int height) {
+		super(x,y,width,height,Color.RED);
+	}
+}
