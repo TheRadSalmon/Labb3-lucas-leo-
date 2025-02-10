6178d8285c976156eb522a51248606ac97a6cf8b
Skurtgodx <leo.brehmer@gmail.com>
2025-02-10 22:27:08 +0100

    Update Game.java

--- GameEngine-v3/Game.java
+++ GameEngine-v3/Game.java
@@ -8,16 +8,23 @@ import java.util.*;
 
 public class Game {
 	private Boll boll;
+	private Bat bat;
+	private SquareCollection fyrkanter;
 	
 	public Game(GameBoard board) {
-	boll = new Boll(300,400,20,Color.ORANGE);
+	boll = new Boll(390,250,20,Color.ORANGE);
+	bat = new Bat(200, 550, 75, 10, Color.RED);
+	fyrkanter = new SquareCollection(50, 0, 20, 20);
 	}
 
 	public void update(Keyboard keyboard) {
-	boll.update(keyboard);
+	boll.update(keyboard, fyrkanter);
+	bat.update(keyboard, fyrkanter);
 	}
 
 	public void draw(Graphics2D graphics) {
 	boll.draw(graphics);
+	bat.draw(graphics);
+	fyrkanter.draw(graphics);
 	}
 }
