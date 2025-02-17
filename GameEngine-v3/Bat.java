5cc89df98e6c5cd7bfad1525246baf656e7c632d
Skurtgodx <leo.brehmer@gmail.com>
2025-02-17 09:05:38 +0100

    Update Bat.java

--- GameEngine-v3/Bat.java
+++ GameEngine-v3/Bat.java
@@ -2,13 +2,16 @@ import java.awt.Graphics2D;
 import java.awt.Color;
 
 
-public class Bat extends Sprite{
+public class Bat extends Sprite {
 	private Color color;
-	public Bat(int x, int y, int width, int height, Color color) {
+	private Boll boll;
+	public Bat(int x, int y, int width, int height, Color color, Boll boll) {
 		super(x, y, width, height);
 		this.color = color;
+		this.boll = boll;
 	}
-	public void update(Keyboard keyboard) {
+	
+	public void update(Keyboard keyboard, SquareCollection square) {
 		if(keyboard.isKeyDown(Key.Left) == true && getX()>0) {
 			setX(getX()-7);
 		}
@@ -16,11 +19,21 @@ public class Bat extends Sprite{
 			setX(getX()+7);
 		}
 		
+		if(boll.getX()<=getX()+getWidth() && boll.getX()>=getX() && (boll.getY())+boll.getWidth()>=getY()) {
+			boll.setyV(-boll.getyV());
+		}/*else if(boll.getY()<=getY()+getHeight() || boll.getY()+(boll.getWidth()) && boll.getY()>= getY() && boll.getX() == getX()+getWidth()) {
+			boll.setxV(-boll.getxV());
+		}*/
 	}
 	public void draw(Graphics2D graphics) {
 		graphics.setColor(color);
 		graphics.fillRect(getX(), getY(), getWidth(), getHeight());
 		graphics.setColor(color.WHITE);
 		graphics.drawString("LBLJ", getX()+23, getY()+9);
+	}
+	
+	public int getBat() {
+		getX();
+		return getX();
 	}
 }
