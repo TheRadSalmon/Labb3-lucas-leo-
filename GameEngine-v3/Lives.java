0444be81418f3417ff81967a7aabc2fe11136d21
Skurtgodx <leo.brehmer@gmail.com>
2025-03-01 18:48:39 +0100

    Create Lives.java

---
+++ GameEngine-v3/Lives.java
@@ -0,0 +1,17 @@
+
+public class Lives {
+	private int lives;
+	
+	public Lives(int startLiv) {
+		this.lives = startLiv;
+	}
+	
+	
+	public int getLives() {
+		return lives;
+	}
+	
+	public void setLives(int lives) {
+		this.lives = lives;
+	}
+}
