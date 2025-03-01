2999284dd98ae3dc14aabb9cf565333bfa3a59b1
Skurtgodx <leo.brehmer@gmail.com>
2025-03-01 18:49:09 +0100

    Create Score.java

---
+++ GameEngine-v3/Score.java
@@ -0,0 +1,15 @@
+
+public class Score {
+	private int score;
+	
+	public Score(int startScore) {
+		this.score = startScore;
+	}
+	
+	public int getScore() {
+		return score;
+		}
+	public void setScore(int score) {
+		this.score = score;
+		}
+}
