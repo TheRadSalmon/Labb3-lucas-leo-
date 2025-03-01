f6b9c2bf9e8620a84f3260a8ae5f0753da57f80a
Skurtgodx <leo.brehmer@gmail.com>
2025-03-01 18:50:57 +0100

    Update Program.java

--- GameEngine-v3/Program.java
+++ GameEngine-v3/Program.java
@@ -1,6 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.Graphics2D; // Man kanske kan ta bort dessa kodrader med import?
 import java.awt.BorderLayout;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
@@ -9,13 +9,16 @@ import javax.swing.*;
 
 public class Program extends JFrame {
 	GameBoard board;
+	//JLabel scoreLabel;
+
 	public Program() {
 		board = new GameBoard();
 		setLayout(new BorderLayout());
 		add(board, BorderLayout.CENTER);
-		add(new JLabel("Highscore"), BorderLayout.WEST);
-		add(new JLabel("Latest runs"), BorderLayout.EAST);
-		add(new JLabel("Score: "), BorderLayout.NORTH); // Här vill jag försöka centrera och justera borderlayouten på mitt nuvarande score. 
+		add(new JLabel("Highscore: " ), BorderLayout.WEST);
+		//scoreLabel = new JLabel("Highscore: ");
+		//add(scoreLabel, BorderLayout.WEST);
+		add(new JLabel("Latest runs: "), BorderLayout.EAST); 
 		setResizable(false);
 		pack();
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -23,6 +26,10 @@ public class Program extends JFrame {
 		board.start();
 	}
 
+	/*public void updateScore(int score) {
+		scoreLabel.setText("Highscore: " + score);
+	}*/
+	
 	@Override
 	protected void processKeyEvent(KeyEvent e) {
 		super.processKeyEvent(e);
