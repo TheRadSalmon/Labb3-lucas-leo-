eff74aad4890567e05dddc0c9fcfaec69d1b1216
Skurtgodx <leo.brehmer@gmail.com>
2025-03-01 18:49:30 +0100

    Update Boll.java

--- GameEngine-v3/Boll.java
+++ GameEngine-v3/Boll.java
@@ -1,12 +1,19 @@
 import java.awt.Color;
-import java.util.Iterator;
 import java.awt.Graphics2D;
 public class Boll extends Sprite {
 	private int tickcount = 0;
 	private Color color;
-	public int xV = 3;
-	public int yV = -4;
-public Boll(int x, int y, int diameter, Color color) {
+	public int xV = 0;
+	public int yV = 0;
+	
+
+	public int bollStartX = 390;
+	public int bollStartY = 250;
+	private int temp = 0;
+	
+	Lives lives = new Lives(3);
+	
+	public Boll(int x, int y, int diameter, Color color) {
 	 super(x,y,diameter,diameter);
 	 this.color = color;
 }
@@ -23,18 +30,42 @@ public Boll(int x, int y, int diameter, Color color) {
 	public void setyV(int yV) {
 		this.yV = yV;
 	}
+	
+	public void resetPos() {
+		setX(bollStartX);
+		setY(bollStartY);
+	}
 
 @Override
-public void update(Keyboard keyboard, SquareCollection square){
+public void update(Keyboard keyboard){
 	tickcount++;
-	if(getX() == 0 && getY()>0) {
-		xV = 3;
+	if(keyboard.isKeyDown(Key.Enter) && lives.getLives() > 0) {
+		int rand = (int)(Math.random()*7)+4;
+		xV = rand;
+		yV = -rand;
+	}
+	if(getX() <= 0 && getY()>0) {
+		xV = Math.abs(xV);
 	}else if(getX()>=780 && getY()>0) {
-		xV = -3;
+		xV = -Math.abs(xV);
 	}else if(getX()!=0 && getX()!= 780 && getY()<=0) {
-		yV = 3;
-	}else if(getY() >=580) {
-		System.exit(0);
+		yV = Math.abs(yV);
+	}else if(getY() >=580 && lives.getLives()>0) {
+		temp = lives.getLives();
+		lives.setLives(lives.getLives()-1);
+		if(temp > lives.getLives() && lives.getLives()>0) {
+			resetPos();
+			xV = 0;
+			yV = 0;
+		}
+		resetPos();
+		if(lives.getLives() <= 0) {
+			xV = 0;
+			yV = 0;
+			
+		}
+		System.out.println("Liv: " + lives.getLives());
+		
 	}
 	
 	if(tickcount % 1 == 0) {
@@ -50,5 +81,6 @@ public void update(Keyboard keyboard, SquareCollection square){
 public void draw(Graphics2D graphics) {
 	 graphics.setColor(color);
 	 graphics.fillOval(getX(),getY(),getWidth(),getHeight());
+	 graphics.drawString("Antal liv: " + lives.getLives(), 390, 530);
 }
 }
