4ea070e82392809ff389fb64cd2ac431428b96f7
Skurtgodx <leo.brehmer@gmail.com>
2025-03-02 14:57:42 +0100

    Update RedBox.java

--- GameEngine-v3/RedBox.java
+++ GameEngine-v3/RedBox.java
@@ -2,7 +2,9 @@ import java.awt.Graphics2D;
 import java.awt.Color;
 
 public class RedBox extends ColoredBox {
-	public RedBox(int x, int y, int width, int height) {
-		super(x,y,width,height,Color.RED);
+	private int hitsRequired;
+	public RedBox(int x, int y, int width, int height, int hitsRequired) {
+		super(x,y,width,height,Color.RED, hitsRequired);
+		this.hitsRequired = hitsRequired;
 	}
 }
