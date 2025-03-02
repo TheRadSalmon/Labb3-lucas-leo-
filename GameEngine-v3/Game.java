74812c61d49010051d3e07be588e835ddb9f167e
Skurtgodx <leo.brehmer@gmail.com>
2025-03-02 13:29:58 +0100

    Update Game.java

--- GameEngine-v3/Game.java
+++ GameEngine-v3/Game.java
@@ -10,6 +10,7 @@ public class Game {
 	private Boll boll;
 	private Bat bat;
 	private SquareCollection fyrkanter;
+	private SquareCollection fyrkanter2;
 	public int bollStartX = 390;
 	public int bollStartY = 250;
 	
@@ -17,11 +18,13 @@ public class Game {
 	boll = new Boll(bollStartX,bollStartY,20,Color.ORANGE); //Ändra alla magic numbers till variabler
 	bat = new Bat(200, 550, 75, 10, Color.RED, boll);
 	fyrkanter = new SquareCollection(50, 0, 55, 20, boll); 
+	fyrkanter2 = new SquareCollection(50, 100, 55, 20, boll);
 	}
 
 	public void update(Keyboard keyboard) {
 	boll.update(keyboard);
 	fyrkanter.update(keyboard, fyrkanter);
+	fyrkanter2.update(keyboard, fyrkanter2);
 	bat.update(keyboard);
 	
 	}
@@ -30,5 +33,6 @@ public class Game {
 	boll.draw(graphics);
 	bat.draw(graphics);
 	fyrkanter.draw(graphics);
+	fyrkanter2.draw(graphics);
 	}
 }
