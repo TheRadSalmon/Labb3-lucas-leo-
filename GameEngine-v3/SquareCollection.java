2c564edfa2838dcf3d2327121543d4627dcb0c65
Skurtgodx <leo.brehmer@gmail.com>
2025-03-02 14:57:13 +0100

    Update SquareCollection.java

--- GameEngine-v3/SquareCollection.java
+++ GameEngine-v3/SquareCollection.java
@@ -8,40 +8,50 @@ import javax.swing.JLabel;
 
 
 
-public class SquareCollection extends ColoredBox{
+public class SquareCollection{
 	private ArrayList<ColoredBox> fyrkanter;
 	private Boll boll;
 	private int score = 0;
-	private Color color;
-	private int rand = (int)(Math.random()*3)+1;
-	private String colorvar = "RED";
-
 	
 	public SquareCollection(int x, int y, int width, int height, Boll boll) {
-		super(x,y,width,height,Color.RED);
 		this.boll = boll; 
 		fyrkanter = new ArrayList<>(10);
 		for(int i = 0; i<10; i++) {
-				fyrkanter.add(new ColoredBox(x + i*75 ,y, width, height, color.RED)); //Jag ändrade dessa variabler till width och height istället för magic numbers så att man även kan ändra ifrån game.
+				int hitsRequired = (int)(Math.random() *3)+1;
+				if(hitsRequired == 1) {
+					fyrkanter.add(new GreenBox(x + i*75 ,y, width, height, hitsRequired));
+				}else if(hitsRequired == 2) {
+					fyrkanter.add(new YellowBox(x + i*75 ,y, width, height, hitsRequired));
+				}else if(hitsRequired == 3) {
+					fyrkanter.add(new RedBox(x + i*75 ,y, width, height, hitsRequired));
+				}
 		}
 	}
 	
+	public int getScore() {
+		return score;
+	}
 	public void update(Keyboard keyboard, SquareCollection Square) {
 
 	    for (int i = fyrkanter.size() - 1; i >= 0; i--) {
+	    	ColoredBox fyrkant = fyrkanter.get(i);
+	    	
 	        Rectangle bollrektangel = new Rectangle(boll.getX(), boll.getY(), boll.getWidth(), boll.getHeight());
 	        Rectangle boxrektangel = new Rectangle(fyrkanter.get(i).getX(), fyrkanter.get(i).getY(), fyrkanter.get(i).getWidth(), fyrkanter.get(i).getHeight());
 
 	        if (bollrektangel.intersects(boxrektangel)) {
+	        	fyrkant.hit();
 	        	
 	        	int fyrkantX = fyrkanter.get(i).getX();
 	            int fyrkantY = fyrkanter.get(i).getY();
 	            int fyrkantWidth = fyrkanter.get(i).getWidth();
 	            int fyrkantHeight = fyrkanter.get(i).getHeight();
 	            
+	            if(fyrkant.isDestroyed() == true) {
+	            	fyrkanter.remove(i);
+	            }
 	            score++;
 	            System.out.println("Score: " + score); // Kodsnutt för att titta om poängen funkar, kan tas bort sen.
-	            fyrkanter.remove(i);
 	            
 	            // vänster
 	            if (boll.getX() + boll.getWidth() - boll.getxV() <= fyrkantX) {
@@ -68,5 +78,6 @@ public class SquareCollection extends ColoredBox{
 	        for (ColoredBox box : fyrkanter) {
 	            box.draw(graphics);
 	        }
+	        graphics.drawString("Poäng: " + score, 390, 545);
 	}
 }
