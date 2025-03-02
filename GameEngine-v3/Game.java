b83dbf33baf817b7da5624273b0a9766b293443a
Skurtgodx <leo.brehmer@gmail.com>
2025-03-02 19:12:16 +0100

    Update Game.java

--- GameEngine-v3/Game.java
+++ GameEngine-v3/Game.java
@@ -9,12 +9,12 @@ import java.util.*;
 public class Game {
 	private Boll boll;
 	private Bat bat;
-	private Lives lives;
 	private SquareCollection fyrkanter;
 	private SquareCollection fyrkanter2;
 	public int bollStartX = 390;
 	public int bollStartY = 250;
 	private int totalScore = 0;
+	private int tempScore = 0;
 	
 	public Game(GameBoard board) {
 	boll = new Boll(bollStartX,bollStartY,20,Color.ORANGE); //Ändra alla magic numbers till variabler
@@ -29,7 +29,7 @@ public class Game {
 	fyrkanter2.update(keyboard, fyrkanter2);
 	bat.update(keyboard);
 	
-	totalScore = fyrkanter.getScore() + fyrkanter2.getScore(); 
+	totalScore = fyrkanter.getScore() + fyrkanter2.getScore() + tempScore; 
 	boll.setTotalScore(totalScore);
 	
 	if(boll.returnLives() <= 0) {
@@ -38,13 +38,25 @@ public class Game {
 	}
 		if(keyboard.isKeyDown(Key.Space) && boll.returnLives()<=0) {
 			fyrkanter = new SquareCollection(50, 0, 55, 20, boll);
+			fyrkanter2 = new SquareCollection(50, 100, 55, 20, boll); //Objektet boll startas inte och på grund av det så får man inga fler liv. Därför startas inte programmet
+			boll.resetLiv();
+		}
+	if(fyrkanter.isEmpty() && fyrkanter2.isEmpty() && boll.returnLives()>0) {
+		boll.resetPos();
+		if(keyboard.isKeyDown(Key.Up) && boll.returnLives()>0) {
+			tempScore = totalScore;
+			fyrkanter = new SquareCollection(50, 0, 55, 20, boll);
 			fyrkanter2 = new SquareCollection(50, 100, 55, 20, boll);
+		}
 	}
 }
 
 	public void draw(Graphics2D graphics) {
 	boll.draw(graphics);
 	bat.draw(graphics);
+	if(fyrkanter.isEmpty() && fyrkanter2.isEmpty() && boll.returnLives()>0) {
+		graphics.drawString("Klicka pil upp för att fortsätta", 390, 450);
+	}
 	fyrkanter.draw(graphics);
 	fyrkanter2.draw(graphics);
 	 graphics.drawString("Total Poäng: " + totalScore, 390, 545);
