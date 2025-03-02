1a96df04808f155b27fa2404f14ac0bca56904bb
Skurtgodx <leo.brehmer@gmail.com>
2025-03-02 14:56:33 +0100

    Update ColoredBox.java

--- GameEngine-v3/ColoredBox.java
+++ GameEngine-v3/ColoredBox.java
@@ -1,16 +1,32 @@
 import java.awt.Color;
 import java.awt.Graphics2D;
 public class ColoredBox extends Sprite {
+	private int hitsRequired;
 	
 	private Color color;
-	public ColoredBox(int x, int y, int width, int height, Color color) {
+	public ColoredBox(int x, int y, int width, int height, Color color, int hitsRequired) {
 		super(x,y,width,height);
 		this.color = color;
+		this.hitsRequired = hitsRequired;
 	}
 	
 	@Override
-	public void update(Keyboard keyboard, SquareCollection square) {
+	public void update(Keyboard keyboard) {
 	
+	}
+	
+	public int getHits() {
+		return hitsRequired;
+	}
+	
+	public void hit() {
+		if(hitsRequired>0) {
+			hitsRequired--;	
+		}
+	}
+	
+	public boolean isDestroyed() {
+		return hitsRequired<=0;
 	}
 	
 	@Override
