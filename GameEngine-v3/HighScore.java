b7b312c46654f03f4e1e2742a837dfe6a17883f0
Skurtgodx <leo.brehmer@gmail.com>
2025-03-16 18:31:24 +0100

    Create HighScore.java

---
+++ GameEngine-v3/HighScore.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+import javax.swing.*;
+
+public class HighScore {
+
+	private DefaultListModel<String> dlm;
+	private JList<String> highScores;
+	private ArrayList<Integer> scores; 
+	
+	public HighScore() {
+		dlm = new DefaultListModel<>();
+		highScores = new JList<>(dlm);
+		scores = new ArrayList<>();
+	}
+	
+	public void addTenScores(int score) {
+		if(scores.size()<10) {
+			scores.add(score);
+			scores.sort(Collections.reverseOrder());
+		}else {
+			int min = Collections.min(scores);
+			if(score>min) {
+				scores.remove(Integer.valueOf(min));
+				scores.add(score);
+			}
+		}
+		scores.sort(Collections.reverseOrder());
+	}
+	
+	
+	public void saveScores() {
+		
+	}
+	
+	public void loadScores() {
+		
+	}
+}
