0a985d341d396d44b939a1870a2675a0e80c490c
TheRadSalmon <lucasamadeus2004@gmail.com>
2025-03-16 14:40:39 +0100

    Create Variables.java

---
+++ GameEngine-v3/Variables.java
@@ -0,0 +1,91 @@
+
+public class Variables {
+//Game.java variablar
+	
+	public static int bollStartX = 390;
+	public static int bollStartY = 250;
+	public static int bollRadius = 20; 
+	
+	//Bat variablar: 
+    public static int bat_X = 200;
+    public static int bat_Y = 550;
+    public static int bat_Width = 75;
+    public static int bat_Height = 10;
+    
+    //Fyrkanter variabler: 
+    public static int fyrkant_X = 50;
+    public static int fyrkant_Y1 = 0; 
+    public static int fyrkant_Y2 = 100;
+    public static int fyrkant_Widht = 55;
+    public static int fyrkant_Height = 20;
+    
+    //Antal liv: 
+    public static int death = 0; 
+    
+    //Draw position: 
+    public static int levaText_X = 390;
+    public static int levaText_Y = 450;
+    public static int scoreText_X = 390; 
+    public static int scoreText_Y = 545;
+    
+    
+//Squarecollection.java variablar: 
+    
+    //Arraylistan: 
+    public static int antalRutor = 10; 
+    
+    //Träff: 
+    public static int maxStuds = 3; 
+    public static int minStuds = 1; 
+    public static int greenHit = 1; 
+    public static int yellowHit = 2; 
+    public static int redHit = 3; 
+    
+    //Rutorna: 
+    public static int mellanRum = 75; 
+    public static int greenPoint = 1; 
+    public static int yellowPoint = 5;
+    public static int redPoint = 10; 
+    
+    //for loopen: 
+    public static int startIndex = 0; 
+    public static int sistaIndex = 1;
+    
+//Bat.java variablar:
+    
+    public static int leftWall = 0; 
+    public static int rightWall = 725;
+    public static int batSpeed = 7;
+    
+    //bat text: 
+    public static int batFontStorlek = 11;
+    public static int batText_X = 23;
+    public static int batText_Y = 9;
+    
+//Boll.java:
+    
+    //boll.text:
+    public static int bollLivFontStorlek = 11;
+    public static int livText_X = 390;
+    public static int livText_Y = 530;
+    public static int gameOverFontStorlek = 100; 
+    public static int gameover_X = 125;
+    public static int gameover_Y = 300;
+    
+    //Boll: 
+    public static int bollLiv = 3; 
+    public static int bollmax_xV = 7; 
+    public static int bollmin_xV = 4; 
+    public static int tickcount = 0; 
+    public static int tickcountTick = 1; 
+    public static int tickcountCompare = 0; 
+    public static int minusLiv = 1;
+    public static int bollStart_xV = 0; 
+    public static int bollStart_yV = 0;
+    public static int bollTemp = 0; 
+    public static int bollLeftWall = 0; 
+    public static int bollRightWall = 780; 
+    public static int bollBottomWall = 580; 
+    public static int bollTopWall = 0; 
+ 
+}
