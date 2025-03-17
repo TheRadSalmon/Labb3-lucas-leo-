9c7243d1b4427b7a3c0dc533fb8f7968742f6bb8
TheRadSalmon <lucasamadeus2004@gmail.com>
2025-03-17 18:16:40 +0100

    Update Program.java

--- GameEngine-v3/Program.java
+++ GameEngine-v3/Program.java
@@ -1,102 +1,99 @@
-import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Graphics2D; // Man kanske kan ta bort dessa kodrader med import?
-import java.awt.BorderLayout;
+import java.awt.*;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
-import java.util.LinkedList;
-
 import javax.swing.*;
 
 public class Program extends JFrame {
-	private GameBoard board;
-	private int score = 0;
-	private JLabel highScoreLabel;
-	private HighScore highScores;
-	private LatestRun latestRun;
-	private String namn = "Leo";
-	private JPanel highPanel;
-	private JPanel runPanel;
-	private JLabel[] highLabel;
-	private JLabel[] runLabel;	
-	
-	public Program() {
-		highScores = new HighScore();
-		latestRun = new LatestRun();
-		board = new GameBoard(this);
-		highPanel = new JPanel();
-		runPanel = new JPanel();
-		
-		setLayout(new BorderLayout(30, 40));
-		
-		highPanel.setLayout(new BoxLayout(highPanel, BoxLayout.Y_AXIS));
-		runPanel.setLayout(new BoxLayout(runPanel, BoxLayout.Y_AXIS));
-		
-		highPanel.add(new JLabel("Highscore"), BorderLayout.WEST);
-		runPanel.add(new JLabel("Latest runs"), BorderLayout.EAST);
-		
-		add(board, BorderLayout.CENTER);
-		add(highPanel,BorderLayout.WEST);
-		add(runPanel,BorderLayout.EAST);
-		
-		highLabel = new JLabel[10];
-		for(int i = 0; i<10; i++) {
-			highLabel[i] = new JLabel("--");
-			highPanel.add(highLabel[i]);
-		}
-		
-		runLabel = new JLabel[3];
-		for(int i = 0; i<3; i++) {
-			runLabel[i] = new JLabel("--");
-			runPanel.add(runLabel[i]);
-		}
-		
-		setResizable(false);
-		pack();
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setVisible(true);
-		board.start();
-	}
-	
-	public void updateScore(int newScore) {
-		score = newScore;
-		System.out.println("Updating score: " + newScore); 
-		highScores.addTenScores(score);
-		latestRun.addScore(score);
-		updateHighScoreDisplay();
-		updateLatestRunDisplay();
-		//System.out.println("Scorelogik var:" + score);
-	}
+    private GameBoard board;
+    private int score = 0;
+    private HighScore highScores;
+    private LatestRun latestRuns;
+    private JPanel highPanel;
+    private JPanel runPanel;
+    private JLabel[] highLabel;
+    private JLabel[] runLabel;
 
-	@Override
-	protected void processKeyEvent(KeyEvent e) {
-		super.processKeyEvent(e);
-		board.processKeyEvent(e);
-	}
+    public Program() {
+        highScores = new HighScore();
+        latestRuns = new LatestRun();
+        board = new GameBoard(this);
+        highPanel = new JPanel();
+        runPanel = new JPanel();
 
-	public static void main(String[] args) {
-		Program program = new Program();
-	} 
-	//Jag tänkte på det, eventuellt nice att ha en action listener om liv == 0 pga då kan man skapa en knapp som "Start game" istället, för lite estetiska anledningar.
-	private void updateHighScoreDisplay() {
-	    ArrayList<Integer> topScores = highScores.getScores();
-	    for (int i = 0; i < highLabel.length; i++) {
-	        if (i < topScores.size()) {
-	            highLabel[i].setText(String.valueOf(topScores.get(i)));
-	        } else {
-	            highLabel[i].setText("--");
-	        }
-	    }
-	}
-	
-	private void updateLatestRunDisplay() {
-	    LinkedList<Integer> latestScores = latestRun.getLatestRuns(); 
-	    for (int i = 0; i < runLabel.length; i++) {
-	        if (i < latestScores.size()) {
-	            runLabel[i].setText(String.valueOf(latestScores.get(i))); 
-	        } else {
-	            runLabel[i].setText("--"); 
-	        }
-	    }
-	}
-}	
+        setLayout(new BorderLayout(30, 40));
+
+        highPanel.setLayout(new BoxLayout(highPanel, BoxLayout.Y_AXIS));
+        runPanel.setLayout(new BoxLayout(runPanel, BoxLayout.Y_AXIS));
+
+        highPanel.add(new JLabel("Highscore"), BorderLayout.WEST);
+        runPanel.add(new JLabel("Latest runs"), BorderLayout.EAST);
+
+        add(board, BorderLayout.CENTER);
+        add(highPanel, BorderLayout.WEST);
+        add(runPanel, BorderLayout.EAST);
+
+        highLabel = new JLabel[10];
+        for (int i = 0; i < 10; i++) {
+            highLabel[i] = new JLabel("--");
+            highPanel.add(highLabel[i]);
+        }
+
+        runLabel = new JLabel[3];
+        for (int i = 0; i < 3; i++) {
+            runLabel[i] = new JLabel("--");
+            runPanel.add(runLabel[i]);
+        }
+
+        setResizable(false);
+        pack();
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        board.start();
+    }
+
+    public void updateScore(int newScore) {
+        score = newScore;
+        System.out.println("Updating score: " + newScore);
+        highScores.addScore(score); // Lägg till poängen i highscore
+        latestRuns.addScore(score); // Lägg till poängen i latest runs
+        updateHighScoreDisplay();
+        updateLatestRunDisplay();
+    }
+
+    @Override
+    protected void processKeyEvent(KeyEvent e) {
+        super.processKeyEvent(e);
+        board.processKeyEvent(e);
+    }
+
+    public static void main(String[] args) {
+        Program program = new Program();
+    }
+
+    private void updateHighScoreDisplay() {
+        // Hämta DefaultListModel från highScores
+        DefaultListModel<String> topScores = highScores.getModel();
+
+        // Uppdatera highLabel med poäng och namn från DefaultListModel
+        for (int i = 0; i < highLabel.length; i++) {
+            if (i < topScores.size()) {
+                highLabel[i].setText(topScores.getElementAt(i)); // Visa "namn: poäng"
+            } else {
+                highLabel[i].setText("--");
+            }
+        }
+    }
+
+    private void updateLatestRunDisplay() {
+        // Hämta DefaultListModel från latestRuns
+        DefaultListModel<String> latestScores = latestRuns.getModel();
+
+        // Uppdatera runLabel med poäng från DefaultListModel
+        for (int i = 0; i < runLabel.length; i++) {
+            if (i < latestScores.size()) {
+                runLabel[i].setText(latestScores.getElementAt(i)); // Visa "Poäng: X"
+            } else {
+                runLabel[i].setText("--");
+            }
+        }
+    }
+}
