daf290889b5df540a1191991c26a48d0718a5f4b
TheRadSalmon <lucasamadeus2004@gmail.com>
2025-03-17 19:30:15 +0100

    Update LatestRun.java

--- GameEngine-v3/LatestRun.java
+++ GameEngine-v3/LatestRun.java
@@ -1,23 +1,18 @@
 import javax.swing.*;
 
 public class LatestRun {
-    private DefaultListModel<String> dlm;  // DefaultListModel för att hantera de senaste poängen
+	private DefaultListModel<String> dlm;
 
-    public LatestRun() {
-        dlm = new DefaultListModel<>();
-    }
-
-    public void addScore(int score) {
-        // Lägg till poängen i DefaultListModel
-        dlm.addElement("Poäng: " + score);
-
-        // Om det finns fler än 3 element, ta bort det äldsta
-        if (dlm.size() > 3) {
-            dlm.removeElementAt(0); // Ta bort det första elementet (äldsta poängen)
-        }
-    }
-
-    public DefaultListModel<String> getModel() {
-        return dlm;
-    }
+	public LatestRun() {
+		dlm = new DefaultListModel<>();
+	}
+	public void addScore(int score) {
+		dlm.add(0,"Poäng: " + score);
+		if (dlm.size() > 3) {
+			dlm.removeElementAt(3);
+		}
+	}
+	public DefaultListModel<String> getModel() {
+		return dlm;
+	}
 }
