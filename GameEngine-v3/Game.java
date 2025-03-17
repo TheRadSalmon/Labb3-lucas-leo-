2efa584e208da1748f0b35976e0823e1413889e8
TheRadSalmon <lucasamadeus2004@gmail.com>
2025-03-17 15:12:30 +0100

    Update Game.java

--- GameEngine-v3/Game.java
+++ GameEngine-v3/Game.java
@@ -11,57 +11,63 @@ public class Game {
 	private Bat bat;
 	private SquareCollection fyrkanter;
 	private SquareCollection fyrkanter2;
-
+	private Boolean gameover = false; 
 	private int totalScore = Variables.gameTotalScore;
 	private int tempScore = Variables.gameTempScore;
-	
+
 	public Game(GameBoard board) {
-	boll = new Boll(Variables.bollStartX,Variables.bollStartY,Variables.bollRadius,Color.ORANGE); 
-	bat = new Bat(Variables.bat_X, Variables.bat_Y, Variables.bat_Width, Variables.bat_Height, Color.RED, boll);
-	fyrkanter = new SquareCollection(Variables.fyrkant_X, Variables.fyrkant_Y1, Variables.fyrkant_Widht, Variables.fyrkant_Height, boll); 
-	fyrkanter2 = new SquareCollection(Variables.fyrkant_X, Variables.fyrkant_Y2, Variables.fyrkant_Widht, Variables.fyrkant_Height, boll);
+		boll = new Boll(Variables.bollStartX,Variables.bollStartY,Variables.bollRadius,Color.ORANGE); 
+		bat = new Bat(Variables.bat_X, Variables.bat_Y, Variables.bat_Width, Variables.bat_Height, Color.RED, boll);
+		fyrkanter = new SquareCollection(Variables.fyrkant_X, Variables.fyrkant_Y1, Variables.fyrkant_Widht, Variables.fyrkant_Height, boll); 
+		fyrkanter2 = new SquareCollection(Variables.fyrkant_X, Variables.fyrkant_Y2, Variables.fyrkant_Widht, Variables.fyrkant_Height, boll);
 	}
 
 	public void update(Keyboard keyboard, Program program) {
-	boll.update(keyboard);
-	fyrkanter.update(keyboard, fyrkanter);
-	fyrkanter2.update(keyboard, fyrkanter2);
-	bat.update(keyboard);
-	
-	totalScore = fyrkanter.getScore() + fyrkanter2.getScore() + tempScore; 
-	
-	if(boll.returnLives() <= Variables.death) {
-		fyrkanter.clearSquares();
-		fyrkanter2.clearSquares();
-	}
+		boll.update(keyboard);
+		fyrkanter.update(keyboard, fyrkanter);
+		fyrkanter2.update(keyboard, fyrkanter2);
+		bat.update(keyboard);
+
+		totalScore = fyrkanter.getScore() + fyrkanter2.getScore() + tempScore; 
+
+		if(boll.returnLives() <= Variables.death && !gameover) {
+			fyrkanter.clearSquares();
+			fyrkanter2.clearSquares();
+			program.updateScore(totalScore); 
+			gameover = true; 
+
+		}
+
 		if(keyboard.isKeyDown(Key.Space) && boll.returnLives()<=Variables.death) {
 			fyrkanter = new SquareCollection(Variables.fyrkant_X, Variables.fyrkant_Y1, Variables.fyrkant_Widht, Variables.fyrkant_Height, boll); 
 			fyrkanter2 = new SquareCollection(Variables.fyrkant_X, Variables.fyrkant_Y2, Variables.fyrkant_Widht, Variables.fyrkant_Height, boll);
 			boll.resetLiv();
 			tempScore = 0;
+			gameover = false; 
 		}
-	if(fyrkanter.isEmpty() && fyrkanter2.isEmpty() && boll.returnLives()>Variables.death) {
-		boll.resetPos();
-		if(keyboard.isKeyDown(Key.Up) && boll.returnLives()>Variables.death) {
-			tempScore = totalScore;
-			fyrkanter = new SquareCollection(Variables.fyrkant_X, Variables.fyrkant_Y1, Variables.fyrkant_Widht, Variables.fyrkant_Height, boll); 
-			fyrkanter2 = new SquareCollection(Variables.fyrkant_X, Variables.fyrkant_Y2, Variables.fyrkant_Widht, Variables.fyrkant_Height, boll);
+
+		if(fyrkanter.isEmpty() && fyrkanter2.isEmpty() && boll.returnLives()>Variables.death) {
+			boll.resetPos();
+			if(keyboard.isKeyDown(Key.Up) && boll.returnLives()>Variables.death) {
+				tempScore = totalScore;
+				fyrkanter = new SquareCollection(Variables.fyrkant_X, Variables.fyrkant_Y1, Variables.fyrkant_Widht, Variables.fyrkant_Height, boll); 
+				fyrkanter2 = new SquareCollection(Variables.fyrkant_X, Variables.fyrkant_Y2, Variables.fyrkant_Widht, Variables.fyrkant_Height, boll);
+			}
 		}
 	}
-}
 
 	public void draw(Graphics2D graphics) {
-	boll.draw(graphics);
-	bat.draw(graphics);
-	if(fyrkanter.isEmpty() && fyrkanter2.isEmpty() && boll.returnLives()>Variables.death) {
-		graphics.drawString("Klicka pil upp för att fortsätta", Variables.levaText_X, Variables.levaText_Y);
+		boll.draw(graphics);
+		bat.draw(graphics);
+		if(fyrkanter.isEmpty() && fyrkanter2.isEmpty() && boll.returnLives()>Variables.death) {
+			graphics.drawString("Klicka pil upp för att fortsätta", Variables.levaText_X, Variables.levaText_Y);
+		}
+		fyrkanter.draw(graphics);
+		fyrkanter2.draw(graphics);
+		graphics.setColor(Color.WHITE);
+		graphics.drawString("Total Poäng: " + totalScore, Variables.scoreText_X, Variables.scoreText_Y);
 	}
-	fyrkanter.draw(graphics);
-	fyrkanter2.draw(graphics);
-	graphics.setColor(Color.WHITE);
-	graphics.drawString("Total Poäng: " + totalScore, Variables.scoreText_X, Variables.scoreText_Y);
-	}
-	
+
 	public int returnTotalScore() {
 		return totalScore;
 	}
