5877483604cc63892a3611e7f0c5134874aae951
TheRadSalmon <lucasamadeus2004@gmail.com>
2025-03-17 18:17:19 +0100

    Update LatestRun.java

--- GameEngine-v3/LatestRun.java
+++ GameEngine-v3/LatestRun.java
@@ -1,17 +1,23 @@
 import javax.swing.*;
-import java.util.*;
 
 public class LatestRun {
-    private LinkedList<Integer> latestRuns = new LinkedList<>();
-  
-    public void addScore(int score) {
-        if (latestRuns.size() >= 3) {
-            latestRuns.poll(); 
-        }
-        latestRuns.add(score);
+    private DefaultListModel<String> dlm;  // DefaultListModel för att hantera de senaste poängen
+
+    public LatestRun() {
+        dlm = new DefaultListModel<>();
     }
 
-    public LinkedList<Integer> getLatestRuns() {
-        return latestRuns;
+    public void addScore(int score) {
+        // Lägg till poängen i DefaultListModel
+        dlm.addElement("Poäng: " + score);
+
+        // Om det finns fler än 3 element, ta bort det äldsta
+        if (dlm.size() > 3) {
+            dlm.removeElementAt(0); // Ta bort det första elementet (äldsta poängen)
+        }
+    }
+
+    public DefaultListModel<String> getModel() {
+        return dlm;
     }
 }
